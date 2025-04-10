46c7e874774011d7a346b2696fe8a5ae6bb9f276
Rohit <rrgawanderohit@gmail.com>
2025-04-10 22:57:36 +0530

    About section done

--- food/res/src/App.jsx
+++ food/res/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
 import Header from "./components/header/Header";
+import About from "./components/about/about";
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
       <Header />
+      <About />
     </div>
   );
 };
