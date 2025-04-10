15a6e3ca9de2fba8ab2ad55d66eeedf8cb8704d1
Rohit <rrgawanderohit@gmail.com>
2025-04-11 01:50:19 +0530

    contact page done

--- food/res/src/App.jsx
+++ food/res/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/header/Header";
 import About from "./components/about/about";
 import Projects from "./components/projects/Project";
 import Testimonials from "./components/testimonial/Testimonials";
+import Contact from "./components/contact/Contact";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <About />
       <Projects />
       <Testimonials />
+      <Contact />
     </div>
   );
 };
