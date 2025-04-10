624d3e21f374b37e9fa1d4987de1dbd0c584ad3d
Rohit <rrgawanderohit@gmail.com>
2025-04-11 00:33:03 +0530

    projects functionality done

--- food/res/src/App.jsx
+++ food/res/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import React from "react";
 import Header from "./components/header/Header";
 import About from "./components/about/about";
+import Projects from "./components/projects/Project";
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
       <Header />
       <About />
+      <Projects />
     </div>
   );
 };
