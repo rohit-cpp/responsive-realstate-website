a8a60c9defc7665e31122c23a6d1c7d5bbcfebe7
Rohit <rrgawanderohit@gmail.com>
2025-04-11 12:42:31 +0530

    contact form and sending email done

--- food/res/src/App.jsx
+++ food/res/src/App.jsx
@@ -5,10 +5,12 @@ import About from "./components/about/about";
 import Projects from "./components/projects/Project";
 import Testimonials from "./components/testimonial/Testimonials";
 import Contact from "./components/contact/Contact";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
+      <ToastContainer />
       <Header />
       <About />
       <Projects />
--- food/res/src/components/contact/Contact.jsx
+++ food/res/src/components/contact/Contact.jsx
@@ -1,6 +1,35 @@
 import React from "react";
+import { toast } from "react-toastify";
 
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "61b8451d-a288-46af-b300-6bedd7c64499");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");
+      toast.success("form submitted successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      toast.error(data.message);
+      toast.error(data.message);
+      setResult(data.message);
+    }
+  };
+
   return (
     <div
       className="text-center p-6 py-20 lg:px-20 w-full overflow-hidden"
@@ -15,7 +44,10 @@ const Contact = () => {
       <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">
         Ready to make your Move? Lets build your future Together
       </p>
-      <form className="max-w-2xl mx-auto text-gray-600 pt-8">
+      <form
+        onSubmit={onSubmit}
+        className="max-w-2xl mx-auto text-gray-600 pt-8"
+      >
         <div className="flex flex-wrap">
           <div className="w-full md:w-1/2 text-left">
             Your Name
@@ -48,7 +80,7 @@ const Contact = () => {
           ></textarea>
         </div>
         <button className="bg-blue-600 text-white py-2 px-12 mb-10 rounded">
-          Send Message
+          {result ? result : "Send Message"}
         </button>
       </form>
     </div>
